fe5d6423e9ec6df71c43b96af1619b8d59b41f9b
Tang Rufus <tangrufus@gmail.com>
2017-04-02 04:26:29 +0800

    Add AbstractHookTestTrait and ActionTest

---
+++ tests/_data/Spy.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TypistTech\WPContainedHook\Test;
+
+class Spy
+{
+    /**
+     * @var int[]
+     */
+    private $invokedParams = [];
+
+    public function plus(int $numberA, int $numberB): int
+    {
+        $this->invokedParams = [$numberA, $numberB];
+        return $numberA + $numberB;
+    }
+
+    public function getInvokedParams(): array
+    {
+        return $this->invokedParams;
+    }
+
+}
--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -1,14 +1,15 @@
 <?php
 // Here you can initialize variables that will be available to your tests.
-
+/**
+ * AspectMock
+ */
 use AspectMock\Kernel;
 
-include codecept_root_dir('vendor/autoload.php'); // composer autoload.
 $kernel = Kernel::getInstance();
 $kernel->init([
     'debug'        => true,
     'includePaths' => [
         codecept_root_dir('src/'),
-        codecept_root_dir('vendor/psr/container/src/'),
+        codecept_root_dir('vendor/league/container/src/'),
     ],
 ]);
