3c330cb05bfa840a4295e30d153a085708955af3
Tang Rufus <tangrufus@gmail.com>
2018-08-26 05:24:31 +0800

    Install codeception

---
+++ tests/unit/_bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once codecept_root_dir('vendor/autoload.php'); // Composer autoload.
+
+// Now call the bootstrap method of WP Mock.
+WP_Mock::activateStrictMode();
+WP_Mock::bootstrap();
